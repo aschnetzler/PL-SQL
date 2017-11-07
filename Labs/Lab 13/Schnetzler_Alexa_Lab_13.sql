998d6b965219d052372f0c93f68aa79b158f5535
Alexa Schnetzler <aschnetzler2014@hotmail.com>
2017-11-06 23:23:41 -0600

    Lab 13
    Finished product of this lab

--- Labs/Lab 13/Schnetzler_Alexa_Lab_13.sql
+++ Labs/Lab 13/Schnetzler_Alexa_Lab_13.sql
@@ -49,32 +49,59 @@
 		FROM bb_Basket;
 --6.4
 	--PROCEDURE
-		CREATE OR REPLACE FUNCTION DAY_ORD_SF 
-			(p_date IN String)
+	CREATE OR REPLACE FUNCTION DAY_ORD_SF 
+			(p_date IN String,
+            p_id IN NUMBER)
 		RETURN STRING AS 
 			lv_weekday STRING(255);
 		BEGIN
 			select to_char(dtcreated, 'DAY')
 			into lv_weekday 
 			From bb_basket
-			where  p_date = dtcreated;
+			where  p_date = dtcreated
+            AND p_id = idbasket;
 			RETURN lv_weekday;
 			Exception when no_data_found then 
 			RETURN 'No date found';
-		END DAY_ORD_SF;
+            END DAY_ORD_SF;
 	--Anon Block
 		DECLARE 
 			lv_weekday STRING(255);
 		BEGIN 
-			lv_weekday := day_ord_SF('23-JAN-16');
+			lv_weekday := day_ord_SF( '23-JAN-12', 3);
 			DBMS_OUTPUT.PUT_LINE(lv_weekday);
 		END;
 	--SQL SELECT
-	SELECT DISTINCT idbasket,  day_ord_sf(dtcreated) AS "Day Created"
-	FROM bb_Basket;
+		SELECT DISTINCT idbasket "Basket Id",  day_ord_sf(dtcreated, idbasket) AS "Day Created"
+		FROM bb_Basket;
 	--SQl GROUP BY
-	SELECT day_ord_sf(dtcreated) AS "Day Created", count(*)
-	FROM bb_Basket
-	GROUP BY day_ord_sf(dtcreated);
+		SELECT DISTINCT day_ord_sf(dtcreated, idbasket) AS "Day Created", count(*) "Number of Purchases"
+		FROM bb_Basket
+		GROUP BY day_ord_sf(dtcreated, idbasket);
 --6.6
+	--Prcedure
+	CREATE OR REPLACE FUNCTION STATUS_DESC_SF 
+	(p_stage IN NUMBER)
+	RETURN VARCHAR2 AS 
+	BEGIN
+	If p_stage =1 Then
+	  RETURN 'Order submitted';
+	  elsif p_stage =2 Then
+	  RETURN 'Accepted, sent to shipping';
+	  elsif p_stage =3 Then
+	  RETURN 'Back-ordered';
+	  elsif p_stage =4 Then
+	  RETURN 'Canceled';
+	  elsif p_stage =5 Then
+	  RETURN 'Shipped';
+	  else RETURN 'Invalid code';
+	  end if;
+	END STATUS_DESC_SF;
+	--Anon Block
+		set pagesize 300;
+		set linesize 300;
+		select status_desc_sf(idstage), idbasket, idstatus, notes, dtstage, shipper, shippingnum
+		from bb_basketstatus
+		where idbasket = 4;
+
 	
